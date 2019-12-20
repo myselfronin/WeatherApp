c53e43a8456b1fd06090e416621d0d05ea90d1f1
Rabinson Ghatani <myself.ronin@hotmail.com>
2019-12-20 21:10:01 +0545

    Setup app for Heroku

---
+++ src/config.js
@@ -0,0 +1,21 @@
+
+/**DARK SKY API
+ *  Register to darksky.net/dev and
+ *  copy the secret key
+ *  paste it to variable darkSkyKey
+ *  https://api.darksky.net/forecast/ab65583546123b70f5d75174b0196ccb/37.8267,-122.4233
+ */
+const darkSkyKey = "ab65583546123b70f5d75174b0196ccb";
+
+/**
+ * MAP BOX
+ * Register to mapbox.com and
+ * copy the secret key provide
+ * paste it to the variable mapboxKey
+ */
+const mapboxKey = "pk.eyJ1IjoibXlzZWxmLXJvbmluIiwiYSI6ImNrMXUxOXJsczA1bjYzY3BheHc3YThmNDkifQ.D-u69vJBCra5SmlFxHMbBg"
+
+module.exports = {
+    darkSkyKey,
+    mapboxKey,
+}
